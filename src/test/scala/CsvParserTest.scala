83b4091879708ee0c458dfbbc9d99d0f8dd37c9d
john bush <john.bush@traxtech.com>
2015-06-03 23:04:12 -0700

    add dquotes to sample

--- src/test/scala/CsvParserTest.scala
+++ src/test/scala/CsvParserTest.scala
@@ -16,7 +16,7 @@ class CsvParserTest extends org.scalatest.WordSpec with org.scalatest.MustMatche
     System.out.println(parsedDoc.toString())
 
     assert(
-      "List(List(heading 1, heading 2, heading 3, heading 4), List(1, 2, 3, 4), List(5, 6, 7, 8), List(9, 10, 11, 12), List(13, 14, 15, 16), List(17, 18, 19, 20), List(woo, dude this is cool, right_on, bro))" ==
+      "List(List(heading 1, heading 2, heading 3, heading 4), List(1, 2, 3, 4), List(5, 6, 7, 8), List(9, 10, 11, 12), List(13, 14, 15, 16), List(17, 18, 19, 20), List(\"woo\", dude this is cool, right_on, bro))" ==
       parsedDoc.toString()
     )
   }
